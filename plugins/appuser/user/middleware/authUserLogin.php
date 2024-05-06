8b44f7017cc4b9f353de5c30e217ce831d0b89a8
Dominik Pallo <xpallo@stuba.sk>
2024-05-06 15:50:41 +0200

    fix problem == restart pc

--- plugins/appuser/user/middleware/authUserLogin.php
+++ plugins/appuser/user/middleware/authUserLogin.php
@@ -3,13 +3,14 @@
 use Closure;
 use Illuminate\Http\Request;
 use AppUser\User\Models\User;
-use AppUser\User\Middleware;
+
 
 // REVIEW: trochu vágne meno, špecifikoval by som podĽa toho čo to robí
 class authUserLogin
 {
     public function handle(Request $request, Closure $next) // REVIEW: podobnú logiku už máš inde
     {
+        throw new \Exception("Funguje :D");
         $token= $request->input('token');
         $user = User::where('token', $token)->first();
 
